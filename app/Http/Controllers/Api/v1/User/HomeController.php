cc3ebbdb75155fbbd419a2b4ab0fcdc5080c49ab
mutasem <mutasemms198@gmail.com>
2026-03-26 00:12:56 +0300

    first commit

--- app/Http/Controllers/Api/v1/User/HomeController.php
+++ app/Http/Controllers/Api/v1/User/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function categories()
     {
         $data = Category::withCount('products')
-            ->orderBy('id', 'asc')->get()
+            ->orderBy('id', 'desc')->get()
             ->map(fn($c) => [
                 'id'             => $c->id,
                 'name_ar'        => $c->name_ar,

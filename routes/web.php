ce60ff73c85610a1ae8087392128696fb2606cee
mutasem <mutasemms198@gmail.com>
2026-04-19 22:27:32 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -68,14 +68,51 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
 
   Route::get('/stream-video/{filename}', function ($filename) {
     $path = base_path('assets/admin/uploads/' . $filename);
-    
+
     if (!file_exists($path)) {
         abort(404);
     }
-    
-    return response()->file($path, [
-        'Content-Type' => 'video/mp4',
-    ]);
+
+    $size     = filesize($path);
+    $start    = 0;
+    $end      = $size - 1;
+    $status   = 200;
+
+    $headers = [
+        'Content-Type'              => 'video/mp4',
+        'Accept-Ranges'             => 'bytes',
+        'Cache-Control'             => 'public, max-age=86400',
+        'Content-Disposition'       => 'inline',
+    ];
+
+    if (request()->hasHeader('Range')) {
+        preg_match('/bytes=(\d+)-(\d*)/', request()->header('Range'), $m);
+        $start  = (int) $m[1];
+        $end    = isset($m[2]) && $m[2] !== '' ? (int) $m[2] : $size - 1;
+        $end    = min($end, $size - 1);
+        $length = $end - $start + 1;
+        $status = 206;
+        $headers['Content-Range']  = "bytes {$start}-{$end}/{$size}";
+        $headers['Content-Length'] = $length;
+    } else {
+        $headers['Content-Length'] = $size;
+    }
+
+    $s = $start;
+    $e = $end;
+
+    return response()->stream(function () use ($path, $s, $e) {
+        $fp     = fopen($path, 'rb');
+        $left   = $e - $s + 1;
+        fseek($fp, $s);
+        while (!feof($fp) && $left > 0) {
+            $chunk = min(1024 * 256, $left);
+            echo fread($fp, $chunk);
+            $left -= $chunk;
+            flush();
+        }
+        fclose($fp);
+    }, $status, $headers);
 })->name('stream.video');
 
 });
